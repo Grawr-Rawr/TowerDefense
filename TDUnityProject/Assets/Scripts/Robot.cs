40861adc883743148ca14d45adfd692d49beb7b3
William George <Friedchiken777@gmail.com>
2016-02-26 18:14:42 -0800

    Robot Tower Placement Method
    -Will
    (Remember to hit 'T' to go into tower building mode upon startup)

---
+++ TDUnityProject/Assets/Scripts/Robot.cs
@@ -0,0 +1,123 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Robot : MonoBehaviour
+{
+    public float moveSpeed, teleSpeed;
+    public float yAdjust, xAdjust;
+
+    // Use this for initialization
+    void Start ()
+    {
+        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    
+	}
+
+    public void MoveAndBuild(Vector3 destination)
+    {
+        StartCoroutine(FloorLogic(destination));
+    }
+
+    int GetCurrentFloor()
+    {
+        for (int i = 0; i < TowerPlacer.Instance.houseLevels.Length; i++)
+        {
+            if (gameObject.transform.position.y - yAdjust == TowerPlacer.Instance.houseLevels[i].yLower)
+            {
+                return i;
+            }
+        }
+        return 0;
+    }
+
+    int SideOf(float compare)
+    {
+        if (gameObject.transform.position.x > compare)
+        {
+            return -1;
+        }
+        else
+        {
+            return 1;
+        }
+    }
+
+    IEnumerator FloorLogic(Vector3 destination)
+    {
+        int floor = GetCurrentFloor();
+        Vector3 newDestination = new Vector3(destination.x - (1 * SideOf(destination.x)), destination.y + yAdjust, destination.z);
+        Vector3 floorChangeDestination;
+        while (TowerPlacer.Instance.robotMovingTime)
+        { 
+            if (gameObject.transform.position.y == newDestination.y)
+            {
+                floor = GetCurrentFloor();
+                yield return StartCoroutine(MoveBuild(newDestination));
+            }
+            else if (gameObject.transform.position.y > newDestination.y)
+            {
+                floor = GetCurrentFloor();
+                floorChangeDestination = new Vector3(TowerPlacer.Instance.houseLevels[floor].waypointDown.x, gameObject.transform.position.y, 0);
+                yield return StartCoroutine(MoveFloor(floorChangeDestination, -1));
+
+            }
+            else if (gameObject.transform.position.y < newDestination.y)
+            {
+                floor = GetCurrentFloor();
+                floorChangeDestination = new Vector3(TowerPlacer.Instance.houseLevels[floor].waypointUp.x, gameObject.transform.position.y, 0);
+                yield return StartCoroutine(MoveFloor(floorChangeDestination, 1));
+            }
+        }
+        TowerPlacer.Instance.PlaceTower(destination, floor);
+        yield return null;
+    }
+
+    IEnumerator MoveBuild(Vector3 end)
+    {
+        Vector3 start = Vector3.zero;
+        while (true)
+        {
+            start = gameObject.transform.position;
+            if (start == end)
+            {
+                break;
+            }
+            gameObject.transform.position = new Vector3(Vector3.MoveTowards(start, end, Time.deltaTime*moveSpeed).x, TowerPlacer.Instance.houseLevels[GetCurrentFloor()].yLower + yAdjust, 0);
+            yield return new WaitForSeconds(Time.deltaTime/moveSpeed);
+        }
+        TowerPlacer.Instance.robotMovingTime = false;
+        TowerPlacer.Instance.towerPlacingTime = true;
+        yield return null;
+    }
+
+    IEnumerator MoveFloor(Vector3 end, int sign)
+    {
+        Vector3 start = Vector3.zero;
+        while (true)
+        {
+            start = gameObject.transform.position;
+            if (start == end)
+            {
+                break;
+            }
+            gameObject.transform.position = new Vector3(Vector3.MoveTowards(start, end, Time.deltaTime * moveSpeed).x, TowerPlacer.Instance.houseLevels[GetCurrentFloor()].yLower + yAdjust, 0);
+            yield return new WaitForSeconds(Time.deltaTime / moveSpeed);
+        }
+        yield return new WaitForSeconds(teleSpeed);
+        int floor = GetCurrentFloor();
+        if (sign == 1)
+        {
+            gameObject.transform.position = new Vector3(TowerPlacer.Instance.houseLevels[floor].waypointUp.y, TowerPlacer.Instance.houseLevels[floor + 1].yLower + yAdjust, 0);
+        }
+        else
+        {
+            gameObject.transform.position = new Vector3(TowerPlacer.Instance.houseLevels[floor].waypointDown.y, TowerPlacer.Instance.houseLevels[floor + -1].yLower + yAdjust, 0);
+        }
+        yield return null;
+    }
+}
--- TDUnityProject/Assets/Scripts/Towers/TowerPlacer.cs
+++ TDUnityProject/Assets/Scripts/Towers/TowerPlacer.cs
@@ -1,26 +1,44 @@
 ﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
 
 public class TowerPlacer : MonoBehaviour
 {
+    const int FALSEWAYPOINTVALUE = 50;
+    public GameObject robotTest;
     public GameObject[] towersToPlace, towerShadows;
     public int towerplacementindex;
-    public bool towerPlacingTime;
-    public int[] xLeft, xRight;
-    public float[] yLower, yUpper;
-    //Direction indicated by 1 for right to left moving enemies and -1 for left to right moving enemies
-    public int[] levelDirection;
+    public bool towerPlacingTime, robotMovingTime;
     //Exceptions are an x and y value where a tower can't be placed
-    public Vector2[] exceptions;
+    public List<Vector2> exceptions;
+    public houseLevel[] houseLevels;
     public float towerXSnapModifier, towerYSnapModifier;
     GameObject towerGhost, previwTowerLocation, previewTower;
     int lastXpos;
     bool drawNewGhost;
+   
+    //Debug stuff
+    public bool viewDebugPositions;
+    public GameObject debugException, debugBound, debugWarp;
+    public List<GameObject> debugObjects;
+
+    public static TowerPlacer Instance { get; private set; }
+
+    void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        Instance = this;
+    }
 
     // Use this for initialization
     void Start ()
     {
         towerPlacingTime = false;
         previwTowerLocation = GameObject.Find("PreviewTower");
+        WarpExceptions();
     }
 	
 	// Update is called once per frame
@@ -42,6 +60,8 @@ public class TowerPlacer : MonoBehaviour
                 Destroy(previewTower.gameObject);
             }
         }
+
+        DebugDraw();
       
         //Check if towers can be placed
         if (towerPlacingTime)
@@ -86,17 +106,17 @@ public class TowerPlacer : MonoBehaviour
                 //if a tower isn't already in clicked position
                 if (mousePosition.collider.gameObject.GetComponent<Towers>() == null)
                 {
-                    for (int i = 0; i < xLeft.Length; i++)
+                    for (int i = 0; i < houseLevels.Length; i++)
                     {
                         //Check if click is in acceptible placment bounds
-                        if (mousePosition.point.x > xLeft[i] && mousePosition.point.x < xRight[i] && mousePosition.point.y > yLower[i] && mousePosition.point.y < yUpper[i])
+                        if (mousePosition.point.x > houseLevels[i].xLeft && mousePosition.point.x < houseLevels[i].xRight && mousePosition.point.y > houseLevels[i].yLower && mousePosition.point.y < houseLevels[i].yUpper)
                         {
                             validPosition = true;
                             bool exceptionFound = false;
-                            Vector3 modifiedPlacement = new Vector3((Mathf.Round(mousePosition.point.x) + towerXSnapModifier), (yLower[i] + towerYSnapModifier), 0);
+                            Vector3 modifiedPlacement = new Vector3((Mathf.Round(mousePosition.point.x) + towerXSnapModifier), (houseLevels[i].yLower + towerYSnapModifier), 0);
                             RaycastHit bufferCheck;
                             //Check for towers personal space
-                            if (Physics.Raycast(modifiedPlacement, Vector3.right * levelDirection[i], out bufferCheck, towersToPlace[towerplacementindex].GetComponent<Towers>().placementBuffer))
+                            if (Physics.Raycast(modifiedPlacement, Vector3.right * houseLevels[i].levelDirection, out bufferCheck, towersToPlace[towerplacementindex].GetComponent<Towers>().placementBuffer))
                             {
                                 if (bufferCheck.collider.gameObject.GetComponent<Towers>() != null)
                                 {
@@ -105,12 +125,12 @@ public class TowerPlacer : MonoBehaviour
                                 }
                             }
                             //Check if tower's range goes out of bounds
-                            if (xRight[i] < (modifiedPlacement.x + towersToPlace[towerplacementindex].GetComponent<Towers>().placementBuffer) && levelDirection[i] == 1)
+                            if (houseLevels[i].xRight < (modifiedPlacement.x + towersToPlace[towerplacementindex].GetComponent<Towers>().placementBuffer) && houseLevels[i].levelDirection == 1)
                             {
                                 //print("Too Far Over");
                                 exceptionFound = true;
                             }
-                            else if (xLeft[i] > (modifiedPlacement.x - towersToPlace[towerplacementindex].GetComponent<Towers>().placementBuffer) && levelDirection[i] == -1)
+                            else if (houseLevels[i].xLeft > (modifiedPlacement.x - towersToPlace[towerplacementindex].GetComponent<Towers>().placementBuffer) && houseLevels[i].levelDirection == -1)
                             {
                                 //print("Too Far Over");
                                 exceptionFound = true;
@@ -128,12 +148,29 @@ public class TowerPlacer : MonoBehaviour
                                 //print("Ghostbusters");
                             }
                             //Looks for exceptions specified within the build bounds that have been designated as no build zones
-                            for (int k = 0; k < exceptions.Length; k++)
+                            for (int k = 0; k < exceptions.Count; k++)
                             {
                                 //check if there is a designed exception on placement
-                                if ((Mathf.Round(mousePosition.point.x) == exceptions[k].x) && (yLower[i] == exceptions[k].y))
+                                if (modifiedPlacement.x == exceptions[k].x && (houseLevels[i].yLower == exceptions[k].y))
                                 {
                                     exceptionFound = true;
+                                }
+                                //check if larger towers fall into the exception
+                                if (houseLevels[i].levelDirection == 1)
+                                {
+                                    if ((modifiedPlacement.x + (towersToPlace[towerplacementindex].GetComponent<Towers>().placementBuffer * houseLevels[i].levelDirection) >= exceptions[k].x)
+                                        && modifiedPlacement.x < exceptions[k].x && houseLevels[i].yLower == exceptions[k].y)
+                                    {
+                                        exceptionFound = true;
+                                    }
+                                }
+                                else
+                                {
+                                    if ((modifiedPlacement.x + (towersToPlace[towerplacementindex].GetComponent<Towers>().placementBuffer * houseLevels[i].levelDirection) <= exceptions[k].x)
+                                        && modifiedPlacement.x > exceptions[k].x && houseLevels[i].yLower == exceptions[k].y)
+                                    {
+                                        exceptionFound = true;
+                                    }
                                 }
                             }
                             if (!exceptionFound)
@@ -142,28 +179,26 @@ public class TowerPlacer : MonoBehaviour
                                 //Build Tower
                                 if (Input.GetMouseButtonDown(0))
                                 {
-                                    GameObject tempTowerHolder;
-                                    tempTowerHolder = Instantiate(towersToPlace[towerplacementindex], modifiedPlacement, transform.rotation) as GameObject;
-                                    if (levelDirection[i] == -1)
-                                    {
-                                        tempTowerHolder.transform.Rotate(0, 180, 0);
-                                    }
-                                    //Kills ghost marker when tower is placed
-                                    if (towerGhost != null)
-                                    {
-                                        Destroy(towerGhost);
-                                        //print("Ghostbusters3");
-                                    }
+                                    robotMovingTime = true;
+                                    towerPlacingTime = false;
+                                    Vector3 modifiedForRobot = new Vector3(modifiedPlacement.x, modifiedPlacement.y - towerYSnapModifier, modifiedPlacement.z);
+                                    robotTest.GetComponent<Robot>().MoveAndBuild(modifiedForRobot);
                                 }
                                 //Draw Ghost tower
                                 else if (drawNewGhost)
                                 {
                                     towerGhost = Instantiate(towerShadows[towerplacementindex], modifiedPlacement, transform.rotation) as GameObject;
-                                    if (levelDirection[i] == -1)
+                                    if (houseLevels[i].levelDirection == -1)
                                     {
                                         towerGhost.transform.Rotate(0, 180, 0);
                                     }
                                 }
+                            }
+                            if (robotMovingTime)
+                            {
+                               // towerPlacingTime = false;
+                                //Vector3 modifiedForRobot = new Vector3(modifiedPlacement.x, modifiedPlacement.y - towerYSnapModifier, modifiedPlacement.z);
+                               // robotTest.GetComponent<Robot>().MoveAndBuild(modifiedForRobot);
                             }
                         }
                     }
@@ -190,4 +225,114 @@ public class TowerPlacer : MonoBehaviour
             }
         }
 	}
+
+    public void PlaceTower(Vector3 modifiedPlacement, int houseLevel)
+    {
+        Vector3 modifiedForTower = new Vector3(modifiedPlacement.x, modifiedPlacement.y + towerYSnapModifier, modifiedPlacement.z);
+        GameObject tempTowerHolder;
+        tempTowerHolder = Instantiate(towersToPlace[towerplacementindex], modifiedForTower, transform.rotation) as GameObject;
+        if (houseLevels[houseLevel].levelDirection == -1)
+        {
+            tempTowerHolder.transform.Rotate(0, 180, 0);
+        }
+        //Kills ghost marker when tower is placed
+        if (towerGhost != null)
+        {
+            Destroy(towerGhost);
+            //print("Ghostbusters3");
+        }
+    }
+
+    //Draws markers for exceptions bounds and warps
+    void DebugDraw()
+    {
+        if (viewDebugPositions)
+        {
+            if (debugObjects.Count == 0)
+            {
+                //Draw exceptions
+                for (int e =0; e < exceptions.Count; e++)
+                {
+                    Vector3 pos = new Vector3(exceptions[e].x, exceptions[e].y, 0);
+                    GameObject temp = Instantiate(debugException, pos, gameObject.transform.rotation) as GameObject;
+                    debugObjects.Add(temp);
+                }
+                for (int f = 0; f < houseLevels.Length; f++)
+                {
+                    //Draw bounds
+                    Vector3 pos = new Vector3(houseLevels[f].xLeft, houseLevels[f].yLower, 0);                    
+                    GameObject temp = Instantiate(debugBound, pos, gameObject.transform.rotation) as GameObject;
+                    debugObjects.Add(temp);
+                    pos = new Vector3(houseLevels[f].xRight, houseLevels[f].yLower, 0);
+                    temp = Instantiate(debugBound, pos, gameObject.transform.rotation) as GameObject;
+                    debugObjects.Add(temp);
+                    //Draw Waypoints
+                    if (houseLevels[f].waypointUp.x < FALSEWAYPOINTVALUE)
+                    {
+                        pos = new Vector3(houseLevels[f].waypointUp.x, houseLevels[f].yLower, 0);
+                        temp = Instantiate(debugWarp, pos, gameObject.transform.rotation) as GameObject;
+                        debugObjects.Add(temp);
+                        pos = new Vector3(houseLevels[f].waypointUp.y, houseLevels[f + 1].yLower, 0);
+                        temp = Instantiate(debugWarp, pos, gameObject.transform.rotation) as GameObject;
+                        debugObjects.Add(temp);
+                    }
+                    if (houseLevels[f].waypointDown.x < FALSEWAYPOINTVALUE)
+                    {
+                        pos = new Vector3(houseLevels[f].waypointDown.x, houseLevels[f].yLower, 0);
+                        temp = Instantiate(debugWarp, pos, gameObject.transform.rotation) as GameObject;
+                        debugObjects.Add(temp);
+                        pos = new Vector3(houseLevels[f].waypointDown.y, houseLevels[f - 1].yLower, 0);
+                        temp = Instantiate(debugWarp, pos, gameObject.transform.rotation) as GameObject;
+                        debugObjects.Add(temp);
+                    }
+                }
+            }
+        }
+        else
+        {
+            if (debugObjects.Count > 0)
+            {
+                foreach (GameObject g in debugObjects)
+                {
+                    Destroy(g);
+                }
+                debugObjects.Clear();
+            }
+        }
+    }
+
+    //Make warps also exceptions
+    void WarpExceptions()
+    {
+        for (int f = 0; f < houseLevels.Length; f++)
+        {
+            if (houseLevels[f].waypointUp.x < FALSEWAYPOINTVALUE)
+            {
+                Vector2 temp = new Vector2(houseLevels[f].waypointUp.x, houseLevels[f].yLower);
+                exceptions.Add(temp);
+                temp = new Vector2(houseLevels[f].waypointUp.y, houseLevels[f + 1].yLower);
+                exceptions.Add(temp);
+            }
+            if (houseLevels[f].waypointDown.x < FALSEWAYPOINTVALUE)
+            {
+                Vector2 temp = new Vector2(houseLevels[f].waypointDown.x, houseLevels[f].yLower);
+                exceptions.Add(temp);
+                temp = new Vector2(houseLevels[f].waypointDown.y, houseLevels[f - 1].yLower);
+                exceptions.Add(temp);
+            }
+        }
+    }
+}
+
+[Serializable]
+public class houseLevel
+{
+    public int xLeft, xRight;
+    public float yLower, yUpper;
+    public int levelDirection;
+    //Direction indicated by 1 for right to left moving enemies and -1 for left to right moving enemies
+    public Vector2 waypointUp, waypointDown;
+    //x values of current level entrance and destination level exit. 
+    //Gets y value from yMin of previous or next element in array. 
+    //Value of 100 indicates no waypoint
 }
